88962d4e7037e0e8a6444ec462ac5b2d214189cd
karimiha <karim.ihadadene@lepoles.com>
2017-03-20 17:05:49 +0100

    diaogue javascript

---
+++ [02]ExoRetrouvailles/js/data.js
@@ -0,0 +1,12 @@
+var data = {
+  'perso_0' : {
+    'name' : 'Chouwi',
+    'picture' : 'img/jerry.gif',
+    'texte' : ['Brouuuuwhaaaaa','Drrrriiiitch','Hinnn Wouaaa']
+  },
+  'perso_1' : {
+    'name' : 'Archibalde',
+    'picture' : 'img/tom.jpg',
+    'texte' : ['Grand père, c\'est moi!','J\'aime les pâttes moi aussi','Pff nimporte quoi!']
+  }
+}
